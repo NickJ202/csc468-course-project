fcc91477cce7fae9d856689b08511fa8fb5b8941
manlalaro1 <odonnellp46@gmail.com>
2022-04-16 10:26:37 -0400

    testing k8 cluster on cloudlab

---
+++ launch_network.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf /users/$USER/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+set -x
+namespaceStatus=$(kubectl get namespaces centigro -o json | jq .status.phase -r)
+
+if [ $namespaceStatus == "Active" ]
+then
+    echo "Namespace ramcoin exists, need to clean up"
+    kubectl delete namespaces centigro
+fi
+
+echo "Creating namespace ramcoin"
+kubectl create namespace centigro 
+ 
+echo "Creating pods"
+kubectl create -f ramcoin.yaml --namespace centigro
+
+echo "Creating services"
+kubectl create -f ramcoin-service.yaml --namespace centigro
+
+
+kubectl get pods -n centigro
+
