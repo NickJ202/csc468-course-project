eab3cdcafc6b1145a247a284878ddda509830e1b
manlalaro1 <odonnellp46@gmail.com>
2022-04-20 12:30:28 -0400

    turn django debug off

--- iam/iam/settings.py
+++ iam/iam/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@=wltfuq08fow8*ou7#ut95um(mn2lb6-ew%(bdavo&a(+sbnm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
