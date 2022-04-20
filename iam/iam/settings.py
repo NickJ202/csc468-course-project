bfeab786710767ee628258ea3e18238fc434a44d
manlalaro1 <odonnellp46@gmail.com>
2022-04-20 12:56:36 -0400

    change image for api

--- iam/iam/settings.py
+++ iam/iam/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         # 'PORT': '5432',
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
         'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
