20a4855adc990ffa48275602a5321fc0758b7a9b
manlalaro1 <odonnellp46@gmail.com>
2022-04-19 23:38:22 -0400

    modify postgres config

--- iam/iam/settings.py
+++ iam/iam/settings.py
@@ -95,9 +95,9 @@ WSGI_APPLICATION = 'iam.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgresdb',
-        'USER': 'postgresadmin',
-        'PASSWORD': 'admin123',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': '5432',
     }
