1f09c739004ff62c40accfd38489150a2c71d724
manlalaro1 <odonnellp46@gmail.com>
2022-04-20 12:02:49 -0400

    change db host

--- iam/iam/settings.py
+++ iam/iam/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'postgres',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
