2705c0b25806db0ba8a0912b9cafd228aa2cb1c5
manlalaro1 <odonnellp46@gmail.com>
2022-04-21 17:58:46 -0400

    changed local run for docker

--- iam/iam/settings.py
+++ iam/iam/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST','postgres'),
+        'HOST': os.getenv('POSTGRES_HOST','127.0.0.1'),
         'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
