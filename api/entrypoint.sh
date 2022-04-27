6f78cd87be9ee490ba2dbf22c9795c07aad7f982
manlalaro1 <odonnellp46@gmail.com>
2022-04-27 09:02:56 -0400

    added docker components

---
+++ api/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py flush --no-input
+python manage.py makemigrations users
+python manage.py makemigrations orgs
+python manage.py makemigrations
+python manage.py migrate users
+python manage.py migrate orgs
+python manage.py migrate 
+python manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 --workers 3 iam.wsgi
+
+exec "$@"
